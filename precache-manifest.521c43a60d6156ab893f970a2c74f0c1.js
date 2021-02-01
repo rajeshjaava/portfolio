5f1cd59b5375662a3ff1e416a0e8fe35ab385893
RajeshAbhimanyu <rajeshthokala10@gmail.com>
2021-02-01 15:46:00 +0530

    Updates

---
+++ precache-manifest.521c43a60d6156ab893f970a2c74f0c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "075249cb3785bbeb083a",
+    "url": "/portfolio/static/css/main.218dcb95.chunk.css"
+  },
+  {
+    "revision": "075249cb3785bbeb083a",
+    "url": "/portfolio/static/js/main.075249cb.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "afb7792a7aafdb18b2ae2ba5e6f05f52",
+    "url": "/portfolio/index.html"
+  }
+];
