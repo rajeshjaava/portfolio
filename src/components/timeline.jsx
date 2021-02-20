516569bb64931b90afa767c141b20223012380fe
RajeshAbhimanyu <rajeshthokala10@gmail.com>
2021-02-20 19:16:44 +0530

    updated timeline

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -15,6 +15,17 @@ export default class Timeline extends Component {
             <div className="row">
               <div className="col-md-12">
                 <div className="timeline-centered">
+                <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Freelancing and Mentorship <span>2017-present</span></h2>
+                        <p>I used to take Mentorship sessions to  college graduates,aspiring software developers to lightening their paths towards achieving their dreams. And also i work for  freelancing projects</p>
+                      </div>
+                    </div>
+                  </article>
                   <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
                     <div className="timeline-entry-inner">
                       <div className="timeline-icon color-3">
@@ -44,7 +55,7 @@ export default class Timeline extends Component {
                       </div>
                       <div className="timeline-label">
                         <h2>Java Developer in CGI<span>2015-2017</span></h2>
-                        <p>Joined as Software Developer , Worked with Bell Canada client for Oracle ASAP product . Developed and Deployed Java Enter Price Applications using Javaa,J2EE,SOAP webservices. </p>
+                        <p>Joined as Software Developer , Worked with Bell Canada client for Oracle ASAP product . Developed and Deployed Java Enter Price Applications using Javaa,J2EE,Spring, Hibernate and SOAP webservices. Contributed in all phases of SDLC . </p>
                       </div>
                     </div>
                   </article>                  
@@ -55,7 +66,7 @@ export default class Timeline extends Component {
                       </div>
                       <div className="timeline-label">
                         <h2>Software Associate in Sabre Travels<span>2014-2015</span></h2>
-                        <p>Joined as Software Developer , Worked for Sabre in house travel applications . Developed and Deployed SOAP webservices using Java,Apache CXF ,SOAP </p>
+                        <p>Joined as Software Developer , Worked for Sabre in house travel applications . Developed and Deployed SOAP webservices using Java,Apache CXF ,SOAP. Vastly used Java,Mockito,WSDL's,Bootstrap and JQuerry  </p>
                       </div>
                     </div>
                   </article>                  
@@ -66,7 +77,7 @@ export default class Timeline extends Component {
                       </div>
                       <div className="timeline-label">
                         <h2>Master Of Computer Applications  <span>2011-2014</span></h2>
-                        <p>I have completed my masters from Sri Venkateswara University, Tirupati where i found real passion on programming and fall in love with developing Andrio Apps,Java Applications. Build and Presented Online Crime Reporting system, Online Fraud Detection System in Java, Dairy and Java tutorial apps using Andriod</p> 
+                        <p>I have completed my masters from Sri Venkateswara University, Tirupati where i found my real passion on programming and fall in love with developing Andriod Apps,Java standalone, web Applications. Build and Presented Online Crime Reporting system, Online Fraud Detection System using Java, Dairy app and Java tutorial apps using Andriod</p> 
                       </div>
                     </div>
                   </article>
